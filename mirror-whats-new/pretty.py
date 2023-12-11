06e15f387c4c16c3fe3686722458d93c1c1f3564
my9oso6gqm <62127466+my9oso6gqm@users.noreply.github.com>
2023-12-11 06:24:20 +0100

    Update pretty.py

--- mirror-whats-new/pretty.py
+++ mirror-whats-new/pretty.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.markdown("""
 # Ciao
-Il mirror al mio [what's new](https://my9oso6gqm.github.io/liste/whats-new.htm).
+Mirror al mio [what's new](https://my9oso6gqm.github.io/liste/whats-new.htm).
 """)
 
 st.markdown("""
