95ae94a7c08be631f8f1749f6b554e68b944f5dd
my9oso6gqm <62127466+my9oso6gqm@users.noreply.github.com>
2023-12-11 02:04:20 +0100

    Update pretty.py

--- mirror-whats-new/pretty.py
+++ mirror-whats-new/pretty.py
@@ -2,9 +2,149 @@ import streamlit as st
 
 st.markdown("""
 # Ciao a tuttu!
-Il mirror al mio [what's new](https://my9oso6gqm.github.io/liste/whats-new.htm):
+Il mirror al mio [what's new](https://my9oso6gqm.github.io/liste/whats-new.htm).
 """)
 
 st.markdown("""
 01-01-2000 - Prova
+""")
+
+st.markdown("""
+01-01-2000 - Prova
+""")
+
+st.markdown("""
+01-01-2000 - Prova
+""")
+
+st.markdown("""
+01-01-2000 - Prova
+""")
+
+st.markdown("""
+01-01-2000 - Prova
+""")
+
+st.markdown("""
+01-01-2000 - Prova
+""")
+
+st.markdown("""
+01-01-2000 - Prova
+""")
+
+st.markdown("""
+01-01-2000 - Prova
+""")
+
+st.markdown("""
+01-01-2000 - Prova
+""")
+
+st.markdown("""
+01-01-2000 - Prova
+""")
+
+st.markdown("""
+01-01-2000 - Prova
+""")
+
+st.markdown("""
+01-01-2000 - Prova
+""")
+
+st.markdown("""
+01-01-2000 - Prova
+""")
+
+st.markdown("""
+01-01-2000 - Prova
+""")
+
+st.markdown("""
+01-01-2000 - Prova
+""")
+
+st.markdown("""
+01-01-2000 - Prova
+""")
+
+st.markdown("""
+01-01-2000 - Prova
+""")
+
+st.markdown("""
+01-01-2000 - Prova
+""")
+
+st.markdown("""
+01-01-2000 - Prova
+""")
+
+st.markdown("""
+01-01-2000 - Prova
+""")
+
+st.markdown("""
+01-01-2000 - Prova
+""")
+
+st.markdown("""
+01-01-2000 - Prova
+""")
+
+st.markdown("""
+01-01-2000 - Prova
+""")
+
+st.markdown("""
+01-01-2000 - Prova
+""")
+
+st.markdown("""
+01-01-2000 - Prova
+""")
+
+st.markdown("""
+01-01-2000 - Prova
+""")
+
+st.markdown("""
+01-01-2000 - Prova
+""")
+
+st.markdown("""
+01-01-2000 - Prova
+""")
+
+st.markdown("""
+01-01-2000 - Prova
+""")
+
+st.markdown("""
+01-01-2000 - Prova
+""")
+
+st.markdown("""
+01-01-2000 - Prova
+""")
+
+st.markdown("""
+01-01-2000 - Prova
+""")
+
+st.markdown("""
+01-01-2000 - Prova
+""")
+
+st.markdown("""
+01-01-2000 - Prova
+""")
+
+st.markdown("""
+01-01-2000 - Prova
+""")
+
+st.markdown("""
+12-12-1999 - Prova
 """)
