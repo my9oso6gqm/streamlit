71f2128ebc48923073aea7dba2831f8d8acfece8
my9oso6gqm <62127466+my9oso6gqm@users.noreply.github.com>
2023-12-13 03:03:57 +0100

    Update pretty.py

--- mirror-whats-new/pretty.py
+++ mirror-whats-new/pretty.py
@@ -6,7 +6,8 @@ Mirror al mio [what's new](https://my9oso6gqm.github.io/liste/whats-new.htm).
 """)
 
 st.markdown("""
-11-12-2023 16:43. Tasso d'interesse annuale effettivo\\
+11-12-2023 16:43 
+## Tasso d'interesse annuale effettivo\\
 Tasso d'interesse annuale effettivo da un investimento di 1 anno:
 """)
 st.latex(r"""
@@ -15,7 +16,8 @@ i = \frac{V(t + 1) - V(t)}{V(t)}
 
 
 st.markdown("""
-11-12-2023 12:00. Prova
+11-12-2023 12:00.
+## Prova
 Lorem ipsum dolor sit amet.
 """)
 
