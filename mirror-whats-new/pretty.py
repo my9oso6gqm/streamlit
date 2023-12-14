b0f1f5cf9ba47876b973ce027e42987954876631
my9oso6gqm <62127466+my9oso6gqm@users.noreply.github.com>
2023-12-14 15:56:44 +0100

    Update pretty.py

--- mirror-whats-new/pretty.py
+++ mirror-whats-new/pretty.py
@@ -8,6 +8,7 @@ Mirror al mio [what's new](https://my9oso6gqm.github.io/liste/whats-new.htm).
 st.markdown(r"""
 ## Tasso d'interesse annuale effettivo
 11-12-2023 16:43  
+  
 Tasso d'interesse annuale effettivo da un investimento di 1 anno:
 $$
 i = \frac{V(t + 1) - V(t)}{V(t)}
