47955500e0216e4abc391790a501102bf7a60041
my9oso6gqm <62127466+my9oso6gqm@users.noreply.github.com>
2023-12-14 15:46:55 +0100

    Update pretty.py

--- mirror-whats-new/pretty.py
+++ mirror-whats-new/pretty.py
@@ -1,34 +1,25 @@
 import streamlit as st
 
-st.markdown("""
+st.markdown(r"""
 # Ciao
 Mirror al mio [what's new](https://my9oso6gqm.github.io/liste/whats-new.htm).
 """)
 
-st.markdown("""
+st.markdown(r"""
 ## Tasso d'interesse annuale effettivo
 11-12-2023 16:43
 \\
 \\
 Tasso d'interesse annuale effettivo da un investimento di 1 anno:
-""")
-st.latex(r"""
+$$
 i = \frac{V(t + 1) - V(t)}{V(t)}
+$$
 """)
 
-st.markdown("""
+st.markdown(r"""
 ## Prova
 11-12-2023 12:00
 \\
 \\
 Lorem ipsum dolor sit amet.
-""")
-
-
-st.markdown(r"""
-## Prova
-11-12-2023 12:00
-$$
-i = \frac{V(t + 1) - V(t)}{V(t)}
-$$
 """)
