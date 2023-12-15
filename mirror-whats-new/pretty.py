abd592ff0a9c7672539744a4488dc33904266902
my9oso6gqm <62127466+my9oso6gqm@users.noreply.github.com>
2023-12-15 16:01:16 +0100

    Update pretty.py

--- mirror-whats-new/pretty.py
+++ mirror-whats-new/pretty.py
@@ -6,6 +6,17 @@ Mirror al mio [what's new](https://my9oso6gqm.github.io/liste/whats-new.htm).
 """)
 
 st.markdown(r"""
+## Alla ricerca della funzione perduta
+15-12-2023 15:58  
+  
+Stavo leggendo il Krugman di microeconomia per cercare una formula sulle curve d'indifferenza dei panieri (Ah, sapevate che paniere, sì quello che usa anche l'Istat, in inglese si traduce in bundle?):
+$$
+\bar{U} = U(q_1,q_2)
+$$
+Mi sono dovuto consultare un Pearson di microeconomia e calculus per trovarla, uffa.
+""")
+
+st.markdown(r"""
 ## Tasso d'interesse annuale effettivo
 11-12-2023 16:43  
   
