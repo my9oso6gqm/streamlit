c10a2d1395c3ad4ed73e26799ba3efcf7604f801
my9oso6gqm <62127466+my9oso6gqm@users.noreply.github.com>
2023-12-20 06:47:50 +0100

    Update and rename streamofconsciousness.py to stream.py

---
+++ mirror-stream/stream.py
@@ -0,0 +1,9 @@
+
+import streamlit as st
+
+st.markdown(r"""
+Mirror al mio [Stream of consciousness](https://my9oso6gqm.github.io/liste/stream.htm).
+
+# Il gruppo hacker russo Lockbit ha rivendicato l’attacco ransomware che negli ultimi dieci giorni ha investito pubbliche amministrazioni in Italia che si avvalgono dei servizi di Westpole.
+
+""")
