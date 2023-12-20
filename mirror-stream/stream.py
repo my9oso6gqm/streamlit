bc833d8e402420c586979d12d3c2d89203fe63f5
my9oso6gqm <62127466+my9oso6gqm@users.noreply.github.com>
2023-12-20 08:50:02 +0100

    Update stream.py

--- mirror-stream/stream.py
+++ mirror-stream/stream.py
@@ -3,10 +3,21 @@ import streamlit as st
 
 st.markdown(r"""
 Mirror al mio [Stream of consciousness](https://my9oso6gqm.github.io/liste/stream.htm).
+""")
 
+st.markdown(r"""
+# Prima plottavo bitcoin mo Apple
+Vado a dormire. Ho tribulato un po' nella speranza di aprire e chiudere un ordine e invece la solita interrogazione delle varie chiusure. Però adesso del NYSE!!! 
+""")
+
+st.image('https://my9oso6gqm.github.io/tmp/201220230840.jpg')
+
+st.markdown(r"""
+Dopo cerco di aprire e chiudere un ordine e fare un'analisi oltre la media mobile.  
+Yep, questo codice ha dei bei salti.
+""")
+
+st.markdown(r"""
 # Il gruppo hacker russo Lockbit ha rivendicato l’attacco ransomware che negli ultimi dieci giorni ha investito pubbliche amministrazioni in Italia che si avvalgono dei servizi di Westpole.
 ilsole
-
-# Prova
-Prova
 """)
