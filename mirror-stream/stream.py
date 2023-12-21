b0d51347715100bccf0a0563b7a197a1d6551acd
my9oso6gqm <62127466+my9oso6gqm@users.noreply.github.com>
2023-12-21 08:04:01 +0100

    Update stream.py

--- mirror-stream/stream.py
+++ mirror-stream/stream.py
@@ -1,4 +1,70 @@
 import streamlit as st
+st.image('https://my9oso6gqm.github.io/mrz.jpg', width=100)
+
+st.markdown(r'''
+Logica matematica, teoria dei numeri, calcolo mentale, giganti sistemi di equazioni, errori nei libri di matematica, liste.
+''')
+
+st.markdown(r'''
+# Ma che noia che Instagram richieda l'uso dell'app per cambiare le impostazioni. Ma soprattutto quale diamine di workaround devo inventarmi per far girare vscode in un docker??? Vabbe, vado a chiudere una posizione buy con python che avevo aperto dall'app di ibkr sul Nikkei.
+''')
+
+st.text('#')
+st.text('#')
+st.text('#')
+
+st.markdown(r"""
+# Piazzare un buy o un sell in ibkr
+[https://ibkrcampus.com/trading-lessons/python-placing-orders/](https://ibkrcampus.com/trading-lessons/python-placing-orders/).
+""")
+
+st.text('#')
+st.text('#')
+st.text('#')
+
+st.markdown(r"""
+# Borse europee ingessate, ma tiene banco risiko tlc
+La conferma che la battaglia contro l’inflazione stia dando i suoi frutti arriva stavolta dal Regno Unito, ma questo non è bastato ad accendere le Borse europee che chiudono fiacche la seduta, con la sola eccezione di Londra, che guadagna oltre l'1%.  
+
+Wall Street in rialzo dopo la pubblicazione di dati economici positivi.  
+
+ilsole
+""")
+
+st.text('#')
+st.text('#')
+st.text('#')
+
+st.markdown(r"""
+# Gayle - Ur just horny 
+[https://youtube.com/watch?v=DWvuD31VG6Q](https://youtube.com/watch?v=DWvuD31VG6Q).
+""")
+
+st.text('#')
+st.text('#')
+st.text('#')
+
+st.markdown(r"""
+# Prima plottavo bitcoin mo Apple
+Vado a dormire. Ho tribulato un po' nella speranza di aprire e chiudere un ordine e invece la solita interrogazione delle varie chiusure. Però adesso del NYSE!!! 
+""")
+
+st.image('https://my9oso6gqm.github.io/tmp/201220230840.jpg')
+
+st.markdown(r"""
+Dopo cerco di aprire e chiudere un ordine e fare un'analisi oltre la media mobile.  
+Yep, questo codice ha dei bei salti.
+""")
+
+st.text('#')
+st.text('#')
+st.text('#')
+
+st.markdown(r"""
+# Il gruppo hacker russo Lockbit ha rivendicato l’attacco ransomware che negli ultimi dieci giorni ha investito pubbliche amministrazioni in Italia che si avvalgono dei servizi di Westpole.
+ilsole
+""")
+import streamlit as st
 
 st.image('https://my9oso6gqm.github.io/mrz.jpg', width=100)
 
