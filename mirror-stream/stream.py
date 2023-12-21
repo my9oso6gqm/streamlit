ea18992b42d6e3b4160bcff6d0fd6a9c164413fa
my9oso6gqm <62127466+my9oso6gqm@users.noreply.github.com>
2023-12-21 06:50:09 +0100

    Update stream.py

--- mirror-stream/stream.py
+++ mirror-stream/stream.py
@@ -4,7 +4,7 @@ import streamlit as st
 col1, col2= st.columns(2)
 
 with col1:
-   st.image("https://static.streamlit.io/examples/cat.jpg", with=200)
+   st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
 
 with col2:
    st.markdown('''Ciao''')
