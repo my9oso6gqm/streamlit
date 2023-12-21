2bb159d45eb8825b2f9d1bae0cb5137040809327
my9oso6gqm <62127466+my9oso6gqm@users.noreply.github.com>
2023-12-21 07:25:13 +0100

    Update stream.py

--- mirror-stream/stream.py
+++ mirror-stream/stream.py
@@ -4,10 +4,18 @@ st.image('https://my9oso6gqm.github.io/mrz.jpg', width=100)
 
 st.markdown('''Logica matematica, teoria dei numeri, calcolo mentale, giganti sistemi di equazioni, errori nei libri di matematica, liste.''')
 
+st.markdown('''
+# Ma che noia che Instagram richieda l'uso dell'app per cambiare le impostazioni. Ma soprattuto quale diamine di workaround devo inventarmi per far girare vscode in un docker???
+''')
+
+st.markdown('#')
+
 st.markdown(r"""
 # Piazzare un buy o un sell in ibkr
 [https://ibkrcampus.com/trading-lessons/python-placing-orders/](https://ibkrcampus.com/trading-lessons/python-placing-orders/).
 """)
+
+st.markdown('#')
 
 st.markdown(r"""
 # Borse europee ingessate, ma tiene banco risiko tlc
@@ -23,6 +31,8 @@ st.markdown(r"""
 [https://youtube.com/watch?v=DWvuD31VG6Q](https://youtube.com/watch?v=DWvuD31VG6Q).
 """)
 
+st.markdown('#')
+
 st.markdown(r"""
 # Prima plottavo bitcoin mo Apple
 Vado a dormire. Ho tribulato un po' nella speranza di aprire e chiudere un ordine e invece la solita interrogazione delle varie chiusure. Però adesso del NYSE!!! 
@@ -34,6 +44,8 @@ st.markdown(r"""
 Dopo cerco di aprire e chiudere un ordine e fare un'analisi oltre la media mobile.  
 Yep, questo codice ha dei bei salti.
 """)
+
+st.markdown('#')
 
 st.markdown(r"""
 # Il gruppo hacker russo Lockbit ha rivendicato l’attacco ransomware che negli ultimi dieci giorni ha investito pubbliche amministrazioni in Italia che si avvalgono dei servizi di Westpole.
