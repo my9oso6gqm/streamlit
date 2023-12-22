ec9c5f18b15ef36d57b55389dfd0f030224893eb
my9oso6gqm <62127466+my9oso6gqm@users.noreply.github.com>
2023-12-22 07:24:51 +0100

    Update stream.py

--- mirror-stream/stream.py
+++ mirror-stream/stream.py
@@ -14,7 +14,7 @@ st.text('')
 st.text('')
 
 
-st.markdown('''<a href='test'>test</a>''')
+st.text('''<a href='test'>test</a>''')
 st.markdown(r'''
 Nuovo stop alla lunga serie positiva dei listini europei sulle prospettive di tagli ai tassi d’interesse da parte delle banche centrali già a partire dal 2024. 
 Si muove rialzo a Wall Street, dopo che Dow Jones e Nasdaq Composite, ieri, hanno vissuto la peggiore giornata da ottobre, lo S&P 500 da settembre.  
