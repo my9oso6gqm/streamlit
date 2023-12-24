62dd0ebcd615f31934d0866664b70620916a4b20
my9oso6gqm <62127466+my9oso6gqm@users.noreply.github.com>
2023-12-24 04:10:27 +0100

    Update stream.py

--- streamofconsciousness/stream.py
+++ streamofconsciousness/stream.py
@@ -20,13 +20,11 @@ Il classico modello sir dal sito della Maa:
 ''')
 
 st.latex(r'''
-\begin{align*}
-\frac{\textit{d}S}{\textit{dt}} = -b s(t) i(t) \\
+\frac{\textit{d}S}{\textit{dt}} = -b\! s(t) i(t) \\
 
 \frac{\textit{d}I}{\textit{dt}} = b s(t) i(t) - k i(t) \\
 
 \frac{\textit{d}R}{\textit{dt}} = k o(t)
-\end{align*}
 ''')
 
 st.markdown(r'''
