82f90efd1065722f208987962ca909d72d49c5cb
my9oso6gqm <62127466+my9oso6gqm@users.noreply.github.com>
2023-12-24 09:59:49 +0100

    Update stream.py

--- streamofconsciousness/stream.py
+++ streamofconsciousness/stream.py
@@ -7,6 +7,39 @@ Maurizio (rjhgshlc2x@gmail.com)
 Logica matematica, teoria dei numeri, calcolo mentale, giganti sistemi di equazioni, errori nei libri di matematica, liste.
 ''')
 
+###
+
+st.markdown(r'''
+
+# 09:58 24-12-2023
+
+## Guerra Israele - Hamas
+
+- Deux mois et demi après le début de la guerre entre Israël et le Hamas, le 7 octobre, le mouvement islamiste a déclaré que les opérations militaires israéliennes dans l’enclave palestinienne ont fait, selon lui, 20 258 morts et plus de 53 000 blessés. Selon le ministère de la santé de Gaza, contrôlé par le Hamas, ce bilan comprend 201 personnes tuées durant les dernières vingt-quatre heures.
+
+- Le porte-parole du ministère du Hamas, Ashraf Al-Qudra, a accusé l’armée israélienne d’avoir « commis plusieurs massacres atroces entraînant la mort de dizaines de personnes dans le camp et la ville de Jabaliya, et dans la zone de Tal Al-Zaatar », dont certains civils « exécutés » selon lui, lors d’une opération terrestre. Il a également affirmé que l’aviation et l’artillerie israéliennes ont visé plusieurs cibles du nord au sud du territoire, dont le camp de réfugiés de Nousseirat (centre) où une frappe a tué 18 personnes.
+
+- Sollicitée par l’Agence France-Presse (AFP), l’armée d’Israël n’a pas spécifiquement répondu aux accusations d’exécutions, mais a assuré que ses frappes « contre des cibles militaires sont conformes aux dispositions du droit international ». Des images de l’AFPTV montrent un corps sous les décombres dans les rues de Jabaliya ainsi que des destructions massives.
+
+- Le président américain Joe Biden a de nouveau appelé samedi le premier ministre israélien, Benyamin Nétanyahou, à protéger les civils dans la bande de Gaza. « Le président a souligné le besoin crucial de protéger la population civile, dont ceux qui contribuent aux opérations d’aide humanitaire, et l’importance de permettre aux civils de quitter en sécurité les zones où les combats continuent de se dérouler », déclare un communiqué de la Maison Blanche.
+
+- Plus tôt dans la journée, M. Biden avait dit aux journalistes de la Maison Blanche qu’il avait eu une « longue discussion » avec M. Nétanyahou, qu’il avait qualifiée de « conversation privée ». En réponse à une question, M. Biden a déclaré : « Je n’ai pas demandé de cessez-le-feu ». Selon le texte publié par Washington, les deux dirigeants ont discuté des « objectifs et du phasage » de la campagne militaire israélienne ainsi que de questions de sécurité.
+
+## Guerra Russia - Ucraina
+
+- Le ministre de la défense ukrainien, Rustem Umerov, a évoqué à la télévision publique ukrainienne la possibilité d’envoyer des convocations militaires en ligne aux ressortissants de son pays qui vivent à l’étranger. Après des propos similaires tenus jeudi, le ministère de la défense ukrainien avait précisé que le ministre parlait de manière générale de l’importance de rejoindre l’armée pour les Ukrainiens, y compris ceux vivant à l’étranger.
+
+- Selon l’état-major ukrainien, « la situation opérationnelle dans l’est et le sud de l’Ukraine reste difficile ». « Au cours de la journée du 23 décembre, l’ennemi a lancé 11 missiles, 14 frappes aériennes, 21 attaques de MLRS [lance-roquettes multiples] sur les positions des troupes ukrainiennes et sur diverses localités », a-t-il détaillé samedi.
+
+- Une attaque de drone dans l’oblast de Kherson a causé la mort d’une personne, a annoncé, samedi, le gouverneur. « Un habitant de 69 ans a succombé à ses blessures », a écrit sur Telegram Oleksandr Prokudin, précisant que l’attaque avait eu lieu sur la ville de Stanislav.
+
+- Au moins sept personnes ont été blessées à Kherson, après un bombardement russe qui a provoqué un incendie, samedi, a fait savoir le gouverneur. Dans la journée, Olexsandr Prokudin avait déjà dit que l’armée russe avait « frappé une infrastructure critique de Kherson » provoquant « une montée en puissance des réseaux de gaz », qui a ensuite déclenché « un incendie ».  
+
+lemonde
+''')
+
+###
+
 
 ### 04:01 24-12-2023
 
