7ed043d14caa1641a398c9e5eaf1135266036762
my9oso6gqm <62127466+my9oso6gqm@users.noreply.github.com>
2023-12-27 10:57:22 +0100

    Update add.py

--- streamofconsciousness/add.py
+++ streamofconsciousness/add.py
@@ -1,10 +1,10 @@
-f = open("i.md", "r")
+f = open("devops/streamofconsciousness/i.md", "r")
 testo = f.read()
 f.close()
 testo1 = "\nst.markdown{r'''\n" + testo + "\n''')\n\n"
 s = open("stream.py", "r")
 righe = s.readlines()
 righe[8] += testo1
-s = open("stream.py", "w")
+s = open("devops/streamofconsciousness/stream.py", "w")
 s.writelines(righe)
 s.close()
