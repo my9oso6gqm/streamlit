7f3a99df621678b5ccc82403b069cee21e04aefe
my9oso6gqm <62127466+my9oso6gqm@users.noreply.github.com>
2023-12-27 12:43:24 +0100

    Update add.py

--- streamofconsciousness/add.py
+++ streamofconsciousness/add.py
@@ -5,6 +5,6 @@ testo1 = "\nst.markdown{r'''\n" + testo + "\n''')\n\n"
 s = open("streamlit/streamofconsciousness/stream.py", "r")
 righe = s.readlines()
 righe[8] += testo1
-s = open("streamlit/streamofconsciousness/.py", "w")
+s = open("streamlit/streamofconsciousness/stream.py", "w")
 s.writelines(righe)
 s.close()
