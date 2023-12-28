169253d7800f21403f0469f840891df86a6cf931
my9oso6gqm <62127466+my9oso6gqm@users.noreply.github.com>
2023-12-28 06:48:53 +0100

    Update stream.py

--- streamofconsciousness/stream.py
+++ streamofconsciousness/stream.py
@@ -10,12 +10,12 @@ Logica matematica, teoria dei numeri, calcolo mentale, giganti sistemi di equazi
 
 st.markdown(r'''
 # 06:46 28-12-2023
-Ascoltando: > Sia - 1+1 (Banx \& Ranx Remix) https://youtube.com/watch?v=LZ0axeyu1Ag  
+Ascoltando: Sia - 1+1 (Banx & Ranx Remix) https://youtube.com/watch?v=LZ0axeyu1Ag  
 Vale sempre questa regola?
 $$3^2 + 4^2 = 5^2$$
 Per scoprirlo dal 1985 al 1992 Wiles ha lavorato ininterrottamente solo a questo problema chiuso in soffitta e trovando la risposta; un no che sa di leggenda.  
   
-Alcuni problemi sembrano semplici. Non vi è motivo per cui non debbano essere facili, eppure si rivelano estremamente intricati.
+Alcuni problemi sembrano semplici. Non vi è motivo per cui non debbano essere facili, eppure si rivelano estremamente intricati.  
 Andrew Wiles
 
 ''')
