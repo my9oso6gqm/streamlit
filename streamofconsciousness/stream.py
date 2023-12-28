643f740035cba903ae66f89a16e3c162fc343edf
my9oso6gqm <62127466+my9oso6gqm@users.noreply.github.com>
2023-12-28 06:51:55 +0100

    Update stream.py

--- streamofconsciousness/stream.py
+++ streamofconsciousness/stream.py
@@ -12,7 +12,7 @@ st.markdown(r'''
 # 06:46 28-12-2023
 Ascoltando: Sia - 1+1 (Banx & Ranx Remix) https://youtube.com/watch?v=LZ0axeyu1Ag  
 Vale sempre questa regola?
-\( 3^2 + 4^2 = 5^2 \)
+$ 3^2 + 4^2 = 5^2 $
 Per scoprirlo dal 1985 al 1992 Wiles ha lavorato ininterrottamente solo a questo problema chiuso in soffitta e trovando la risposta; un no che sa di leggenda.  
   
 Alcuni problemi sembrano semplici. Non vi è motivo per cui non debbano essere facili, eppure si rivelano estremamente intricati.  
