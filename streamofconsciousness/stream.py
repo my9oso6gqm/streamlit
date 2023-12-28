7a965ab0924297258773dc01a00eb1bbdd93030f
my9oso6gqm <62127466+my9oso6gqm@users.noreply.github.com>
2023-12-28 17:02:16 +0100

    Update stream.py

--- streamofconsciousness/stream.py
+++ streamofconsciousness/stream.py
@@ -4,7 +4,7 @@ st.image('https://my9oso6gqm.github.io/mrz.jpg', width=100)
 
 st.markdown(r'''
 Maurizio (mrzgrd557@gmail.com)  
-Logica matematica, teoria dei numeri, calcolo mentale, giganti sistemi di equazioni, errori nei libri di matematica, [liste](https://my9oso6gqm.github.io/liste/).
+Logica matematica, teoria dei numeri, calcolo mentale, giganti sistemi di equazioni, errori nei libri di matematica, liste.
 ''')
 
 
