ed85193d24c34f0b6df0a84ba3171622bfe0d30b
my9oso6gqm <62127466+my9oso6gqm@users.noreply.github.com>
2023-12-28 06:56:13 +0100

    Update stream.py

--- streamofconsciousness/stream.py
+++ streamofconsciousness/stream.py
@@ -11,6 +11,7 @@ Logica matematica, teoria dei numeri, calcolo mentale, giganti sistemi di equazi
 st.markdown(r'''
 # 06:46 28-12-2023
 Ascoltando: Sia - 1+1 (Banx & Ranx Remix) https://youtube.com/watch?v=LZ0axeyu1Ag  
+  
 Vale sempre questa regola?
 $$
 3^2 + 4^2 = 5^2
