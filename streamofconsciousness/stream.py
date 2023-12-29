e3c39a154d402609e02a5113bae505c49861b014
my9oso6gqm <62127466+my9oso6gqm@users.noreply.github.com>
2023-12-29 13:55:06 +0100

    Update stream.py

--- streamofconsciousness/stream.py
+++ streamofconsciousness/stream.py
@@ -10,7 +10,8 @@ Logica matematica, teoria dei numeri, calcolo mentale, giganti sistemi di equazi
 
 st.markdown(r'''
 # 13:51 29-12-2023
-Con i futures di Wall Street in leggero rialzo (+0,09% quello sul Dow Jones e +0,13% quello sull’S&P500), gli indici europei si confermano positivi, anche il Ftse Mib di Piazza Affari, in rialzo dello 0,40% a 30.452 punti grazie alla buona intonazione di Ferrari, Recordati, Erg e Unicredit. Lo spread Btp/Bund risale leggermente a 164,9 punti base. A tenere banco è la notizia che i leader del G7 discuteranno una nuova strategia legale che consentirà il sequestro di 300 miliardi di dollari di asset russi congelati, quando si incontreranno a febbraio, secondo quanto riferito due fonti a conoscenza dei piani e un funzionario britannico a Reuters.
+Con i futures di Wall Street in leggero rialzo (+0,09% quello sul Dow Jones e +0,13% quello sull’S&P500), gli indici europei si confermano positivi, anche il Ftse Mib di Piazza Affari, in rialzo dello 0,40% a 30.452 punti grazie alla buona intonazione di Ferrari, Recordati, Erg e Unicredit. Lo spread Btp/Bund risale leggermente a 164,9 punti base. A tenere banco è la notizia che i leader del G7 discuteranno una nuova strategia legale che consentirà il sequestro di 300 miliardi di dollari di asset russi congelati, quando si incontreranno a febbraio, secondo quanto riferito due fonti a conoscenza dei piani e un funzionario britannico a Reuters.  
+mf
 
 ''')
 
